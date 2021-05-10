d9f77d391eb110e7a2f8692e9a4ddc48cb4645f5
Jorge Andrés Mena <jormencar@yahoo.com>
2021-05-10 14:56:37 -0400

    ADD: header

---
+++ src/components/Header.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a href="">
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+        <a href="">
+          <img src="/images/search-icon.svg" alt="" />
+          <span>SEARCH</span>
+        </a>
+        <a href="">
+          <img src="/images/watchlist-icon.svg" alt="" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="">
+          <img src="/images/original-icon.svg" alt="" />
+          <span>ORIGINALS</span>
+        </a>
+        <a href="">
+          <img src="/images/movie-icon.svg" alt="" />
+          <span>MOVIES</span>
+        </a>
+        <a href="">
+          <img src="/images/series-icon.svg" alt="" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="https://lh3.googleusercontent.com/ogw/ADGmqu_JhLU0I44brv1UxBt6n-f0xB7WvCkoa3YI9bCtvw=s32-c-mo" />
+    </Nav>
+  );
+}
+
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 20px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+    img {
+      height: 20px;
+    }
+    span {
+      position: relative;
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      &:after {
+        content: '';
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -9px;
+        opacity: 0;
+        transform: scaleX(0);
+        transition: all 250ms cubic-bezier(0.25, 0.56, 0.45, 0.94) 0s;
+      }
+    }
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
